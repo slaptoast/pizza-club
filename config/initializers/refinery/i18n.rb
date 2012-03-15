0975a73c18ee6b48bdc3c50d839450dde63cdc3d
Terry Johnson <slaptoast@gmail.com>
2012-03-14 22:22:12 -0500

    here is all this stuff

---
+++ config/initializers/refinery/i18n.rb
@@ -0,0 +1,15 @@
+# encoding: utf-8
+
+Refinery::I18n.configure do |config|
+  # config.enabled = true
+
+  # config.default_locale = :en
+
+  # config.current_locale = :en
+
+  # config.default_frontend_locale = :en
+
+  # config.frontend_locales = [:en]
+
+  # config.locales = {:ja=>"日本語", :sl=>"Slovenian", :fr=>"Français", :"zh-CN"=>"Simplified Chinese", :sv=>"Svenska", :da=>"Dansk", :bg=>"Български", :es=>"Español", :"zh-TW"=>"Traditional Chinese", :pl=>"Polski", :"pt-BR"=>"Português", :it=>"Italiano", :nl=>"Nederlands", :rs=>"Srpski", :de=>"Deutsch", :cs=>"Česky", :lv=>"Latviski", :sk=>"Slovenský", :nb=>"Norsk Bokmål", :en=>"English", :el=>"Ελληνικά", :ru=>"Русский"}
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PizzaClub::Application.config.secret_token = '692e77a9cd8c68533efa09f48ae50be2b5e1e69fa894c716064e3404f661ddd3c009db8d8f713a0da26bcf3c5e69879e6e86d55862e25513322a9cc493fc067b'
